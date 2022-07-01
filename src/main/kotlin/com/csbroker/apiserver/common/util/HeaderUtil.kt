6035ab72d5c1fe153567f0dbf241ceac65422ce6
kshired <shiroed1211@naver.com>
2022-07-01 17:35:07 +0900

    🚧 Add utils for cookie values

---
+++ src/main/kotlin/com/csbroker/apiserver/common/util/HeaderUtil.kt
@@ -0,0 +1,20 @@
+package com.csbroker.apiserver.common.util
+
+import javax.servlet.http.HttpServletRequest
+
+const val HEADER_AUTHORIZATION = "Authorization"
+const val TOKEN_PREFIX = "Bearer "
+
+class HeaderUtil {
+    companion object {
+        fun getAccessToken(request: HttpServletRequest): String? {
+            val headerValue = request.getHeader(HEADER_AUTHORIZATION)
+
+            headerValue?.let {
+                return if (headerValue.startsWith(TOKEN_PREFIX)) headerValue.substring(TOKEN_PREFIX.length) else null
+            }.let {
+                return null
+            }
+        }
+    }
+}
