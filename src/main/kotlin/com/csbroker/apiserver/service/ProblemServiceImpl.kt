e52016c9f16efa80a3c6cd005b537e38a5bc3320
kshired <shiroed1211@naver.com>
2022-07-06 18:33:47 +0900

    💩 Implement problem service interface - Problem Service interface 구현 - Paging 관련하여 수정 필요 -- Application side에서 filtering 하는 로직을 query로 수정 필요

---
+++ src/main/kotlin/com/csbroker/apiserver/service/ProblemServiceImpl.kt
@@ -0,0 +1,41 @@
+package com.csbroker.apiserver.service
+
+import com.csbroker.apiserver.dto.ProblemDetailResponseDto
+import com.csbroker.apiserver.dto.ProblemResponseDto
+import com.csbroker.apiserver.dto.ProblemSearchDto
+import com.csbroker.apiserver.model.Problem
+import com.csbroker.apiserver.model.ProblemTag
+import com.csbroker.apiserver.model.Tag
+import com.csbroker.apiserver.repository.ProblemRepository
+import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class ProblemServiceImpl : ProblemService {
+    @Autowired
+    private lateinit var problemRepository: ProblemRepository
+
+    @Autowired
+    private lateinit var queryFactory: SpringDataQueryFactory
+
+    override fun findProblems(problemSearchDto: ProblemSearchDto): List<ProblemResponseDto> {
+        return problemRepository.findByTitleContainingIgnoreCase(problemSearchDto.query)
+            .filter {
+                it.problemTags.map(ProblemTag::tag).map(Tag::name)
+                    .toList().containsAll(problemSearchDto.tags)
+            }.filter {
+                it.gradingHistory.any { gh ->
+                    gh.problem == it && gh.user.username == problemSearchDto.solvedBy
+                }
+            }.sortedByDescending {
+                it.createdAt
+            }.map(Problem::toProblemResponseDto)
+    }
+
+    override fun findProblemById(id: UUID): ProblemDetailResponseDto? {
+        return problemRepository.findByIdOrNull(id)?.toProblemDetailResponseDto()
+    }
+}
