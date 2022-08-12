1549b97682b5892727807d60f5abf9c1a93e754e
kshired <shiroed1211@naver.com>
2022-08-12 13:12:08 +0900

    :green_heart: Remove unused import

--- src/main/kotlin/com/csbroker/apiserver/service/CustomUserDetailsService.kt
+++ src/main/kotlin/com/csbroker/apiserver/service/CustomUserDetailsService.kt
@@ -5,7 +5,6 @@ import com.csbroker.apiserver.common.exception.EntityNotFoundException
 import com.csbroker.apiserver.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
