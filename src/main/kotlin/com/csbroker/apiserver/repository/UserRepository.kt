45414804401230dd85b5db127601c0e901aa46ca
kshired <shiroed1211@gmail.com>
2022-09-02 16:44:21 +0900

    :passport_control: Add check for distinguishing deleted user

--- src/main/kotlin/com/csbroker/apiserver/repository/UserRepository.kt
+++ src/main/kotlin/com/csbroker/apiserver/repository/UserRepository.kt
@@ -3,11 +3,20 @@ package com.csbroker.apiserver.repository
 import com.csbroker.apiserver.common.enums.Role
 import com.csbroker.apiserver.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface UserRepository : JpaRepository<User, UUID> {
-    fun findByEmail(email: String): User?
+    @Query("select u from User u where u.email = :email and u.isDeleted = FALSE")
+    fun findByEmail(@Param("email") email: String): User?
+
+    @Query("select u from User u where u.id = :id and u.isDeleted = FALSE")
+    fun findByIdOrNull(@Param("id") id: UUID): User?
+
     fun findByEmailOrUsername(email: String, username: String): User?
+
     fun findUsersByRole(role: Role): List<User>
+
     fun findUserByProviderId(providerId: String): User?
 }
