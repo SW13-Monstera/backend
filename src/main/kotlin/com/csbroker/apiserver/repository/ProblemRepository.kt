0f326445d5da1384a397332a3054ab7e580e4cb4
kshired <shiroed1211@gmail.com>
2022-09-14 16:13:50 +0900

    :card_file_box: Modify gradableProblem count condition

--- src/main/kotlin/com/csbroker/apiserver/repository/ProblemRepository.kt
+++ src/main/kotlin/com/csbroker/apiserver/repository/ProblemRepository.kt
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query
 interface ProblemRepository : JpaRepository<Problem, Long>, ProblemRepositoryCustom {
     fun deleteProblemsByIdIn(ids: List<Long>)
 
-    @Query("select count(p) from Problem p where p.isGradable = TRUE")
+    @Query("select count(p) from Problem p where p.isGradable = TRUE and p.isActive = TRUE")
     fun countGradableProblem(): Long
 }
