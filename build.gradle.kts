008c0b7e60a22dbd90c859671d9ba360e05402a1
kshired <shiroed1211@gmail.com>
2022-09-15 14:54:46 +0900

    :heavy_plus_sign: Add aws ses sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,6 +83,8 @@ dependencies {
     // mail
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("aws.sdk.kotlin:ses:0.16.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
 dependencyManagement {
