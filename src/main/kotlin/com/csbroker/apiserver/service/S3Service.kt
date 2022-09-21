bff15d136e5e7effefbf87f45c6b5673a3501cd6
kshired <shiroed1211@gmail.com>
2022-09-21 18:16:01 +0900

    :rotating_light: Fix linter warnings

--- src/main/kotlin/com/csbroker/apiserver/service/S3Service.kt
+++ src/main/kotlin/com/csbroker/apiserver/service/S3Service.kt
@@ -3,5 +3,5 @@ package com.csbroker.apiserver.service
 import org.springframework.web.multipart.MultipartFile
 
 interface S3Service {
-    suspend fun uploadProfileImg(multipartFile: MultipartFile) : String
+    suspend fun uploadProfileImg(multipartFile: MultipartFile): String
 }
