2dc94ec7f142f4fbeb87e6d799f027152ed1f9e3
kshired <shiroed1211@gmail.com>
2022-10-14 01:01:51 +0900

    :recycle: Remove any type

--- src/main/kotlin/io/csbroker/apiserver/dto/notification/NotificationResponseDto.kt
+++ src/main/kotlin/io/csbroker/apiserver/dto/notification/NotificationResponseDto.kt
@@ -3,6 +3,6 @@ package io.csbroker.apiserver.dto.notification
 data class NotificationResponseDto(
     val id: Long,
     val content: String,
-    val link: Any,
+    val link: String,
     val isRead: Boolean,
 )
