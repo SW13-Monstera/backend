caef463bed63af335dee0966e3efc5667eb732e6
kshired <shiroed1211@gmail.com>
2022-11-14 00:22:02 +0900

    :sparkles: [MSTR-364] feat: add consist of field to short problem detail api

---
+++ src/main/kotlin/io/csbroker/apiserver/controller/v2/response/ShortProblemDetailResponseV2Dto.kt
@@ -0,0 +1,20 @@
+package io.csbroker.apiserver.controller.v2.response
+
+class ShortProblemDetailResponseV2Dto(
+    val id: Long,
+    val title: String,
+    val tags: List<String>,
+    val description: String,
+    val correctSubmission: Int,
+    val correctUserCnt: Int,
+    val totalSubmission: Int,
+    val answerLength: Int,
+    val consistOf: ShortProblemAnswerType,
+    val isSolved: Boolean
+)
+
+enum class ShortProblemAnswerType {
+    ENGLISH,
+    KOREAN,
+    NUMERIC
+}
