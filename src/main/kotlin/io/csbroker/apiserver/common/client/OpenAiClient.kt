46eeb84cc07ecebb9e732219e4ade4b626406221
kshired <shiroed1211@gmail.com>
2023-03-12 18:34:49 +0900

    fix : fix typo

--- src/main/kotlin/io/csbroker/apiserver/common/client/OpenAiClient.kt
+++ src/main/kotlin/io/csbroker/apiserver/common/client/OpenAiClient.kt
@@ -4,13 +4,13 @@ import io.csbroker.apiserver.common.client.request.ChatRequestDto
 import io.csbroker.apiserver.common.client.response.ChatResponseDto
 import io.csbroker.apiserver.common.util.HEADER_AUTHORIZATION
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "openai", url = "https://api.openai.com")
 interface OpenAiClient {
-    @GetMapping("/v1/chat/completions")
+    @PostMapping("/v1/chat/completions")
     fun chatCompletion(
         @RequestHeader(HEADER_AUTHORIZATION) apiKey: String,
         @RequestBody chatRequestDto: ChatRequestDto,
--- src/main/kotlin/io/csbroker/apiserver/service/ChatServiceImpl.kt
+++ src/main/kotlin/io/csbroker/apiserver/service/ChatServiceImpl.kt
@@ -2,18 +2,19 @@ package io.csbroker.apiserver.service
 
 import io.csbroker.apiserver.common.client.OpenAiClient
 import io.csbroker.apiserver.common.client.request.ChatRequestDto
+import io.csbroker.apiserver.common.util.TOKEN_PREFIX
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ChatServiceImpl(
     private val openAiClient: OpenAiClient,
-    @Value("\${openai.apikey}")
+    @Value("\${openai.key}")
     private val apiKey: String,
 ) : ChatService {
     override fun completeChat(email: String, content: String): String {
         val response = openAiClient.chatCompletion(
-            apiKey,
+            "$TOKEN_PREFIX $apiKey",
             ChatRequestDto(
                 messages = listOf(
                     ChatRequestDto.ChatRequestMessage(content = content, user = email),
