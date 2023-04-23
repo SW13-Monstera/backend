3c9a94699090eab77994f76d967ea4d21605cf31
kshired <shiroed1211@gmail.com>
2023-04-23 21:28:06 +0900

    hotfix : problem tag id 생성 전략 수정

--- src/main/kotlin/io/csbroker/apiserver/model/ProblemTag.kt
+++ src/main/kotlin/io/csbroker/apiserver/model/ProblemTag.kt
@@ -4,6 +4,7 @@ import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
@@ -13,7 +14,7 @@ import javax.persistence.Table
 @Table(name = "problem_tag")
 class ProblemTag(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "problem_tag_id")
     val id: Long? = null,
 
--- src/main/kotlin/io/csbroker/apiserver/service/problem/TagUpserter.kt
+++ src/main/kotlin/io/csbroker/apiserver/service/problem/TagUpserter.kt
@@ -24,6 +24,7 @@ class TagUpserter(
             ProblemTag(problem = problem, tag = it)
         }
 
+        problemTagRepository.saveAll(problemTags)
         problem.problemTags.addAll(problemTags)
     }
 
