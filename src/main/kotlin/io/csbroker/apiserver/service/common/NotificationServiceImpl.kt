b82ba9cbb19a693b33ec93229762145f98ea5767
SEONGILKIM <shiroed1211@naver.com>
2023-06-04 22:29:05 +0900

    test : 알림 생성 API 테스트 추가 (#119)
    * feat : 통합테스트에서 사용할 유틸함수 및 기반 클래스 추가
    
    * fix : user가 한번만 생성 되도록 수정
    
    * update secrets
    
    * test : 문제 세트 어드민 API 테스트 코드 추가 (#117)
    
    * refactor : 버그 수정
    
    * test : 알림 생성 API 테스트 추가
    
    * fix : bug
    
    * refactor : 리뷰반영
    
    * refactor : 리뷰반영

--- src/main/kotlin/io/csbroker/apiserver/service/common/NotificationServiceImpl.kt
+++ src/main/kotlin/io/csbroker/apiserver/service/common/NotificationServiceImpl.kt
@@ -28,8 +28,16 @@ class NotificationServiceImpl(
 
     @Transactional
     override fun createBulkNotification(notificationRequestListDto: List<NotificationRequestDto>): Int {
-        notificationRepository.insertBulkNotifications(notificationRequestListDto)
-        return notificationRequestListDto.size
+        val requestUserIds = notificationRequestListDto.map { it.userId }.toSet()
+        val users = userRepository.findAllById(requestUserIds)
+
+        if (users.size != requestUserIds.size) {
+            throw EntityNotFoundException("존재하지 않는 유저가 요청에 포함되어 있습니다.")
+        }
+
+        return notificationRepository.saveAll(
+            notificationRequestListDto.map { Notification(it, users.find { u -> u.id == it.userId }!!) },
+        ).size
     }
 
     override fun getNotification(email: String, page: Pageable): Page<Notification> {
---
+++ src/test/kotlin/io/csbroker/apiserver/controller/v1/admin/AdminControllerIntegrationTest.kt
@@ -0,0 +1,117 @@
+package io.csbroker.apiserver.controller.v1.admin
+
+import com.jayway.jsonpath.JsonPath
+import io.csbroker.apiserver.auth.ProviderType
+import io.csbroker.apiserver.common.enums.Role
+import io.csbroker.apiserver.controller.IntegrationTest
+import io.csbroker.apiserver.dto.notification.NotificationBulkInsertDto
+import io.csbroker.apiserver.dto.notification.NotificationRequestDto
+import io.csbroker.apiserver.model.Notification
+import io.csbroker.apiserver.model.User
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpMethod
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.UUID
+
+class AdminControllerIntegrationTest : IntegrationTest() {
+    @Test
+    fun `어드민 유저 목록 조회`() {
+        // given + when
+        val response = request(
+            method = HttpMethod.GET,
+            url = "/api/admin/users/admin",
+            isAdmin = true,
+        )
+
+        // then
+        response.andExpect(status().isOk)
+            .andExpect {
+                val id = JsonPath.read(it.response.contentAsString, "$.data[0].id") as String
+                val user = findOne<User>("SELECT u From User u where u.id = :id", mapOf("id" to UUID.fromString(id)))
+                user.role shouldBe Role.ROLE_ADMIN
+            }
+    }
+
+    @Test
+    fun `단일 알림 생성`() {
+        // given
+        val user = save(
+            User(
+                email = "test-noti@test.com",
+                username = "test-noti",
+                password = "test-noti",
+                providerType = ProviderType.LOCAL,
+            ),
+        )
+
+        // when
+        val response = request(
+            method = HttpMethod.POST,
+            url = "/api/admin/notification",
+            body = NotificationRequestDto(
+                content = "test",
+                userId = user.id!!,
+                link = "https://test.com",
+            ),
+            isAdmin = true,
+        )
+
+        // then
+        response.andExpect(status().isOk)
+            .andExpect {
+                val id = JsonPath.read(it.response.contentAsString, "$.data.id") as Int
+                val notification = findOne<Notification>(
+                    "SELECT n FROM Notification n WHERE n.id = :id",
+                    mapOf("id" to id.toLong()),
+                )
+                notification.content shouldBe "test"
+                notification.link shouldBe "https://test.com"
+                notification.user.id shouldBe user.id
+            }
+    }
+
+    @Test
+    fun `알림 여러개 생성`() {
+        // given
+        val user = save(
+            User(
+                email = "test-noti2@test.com",
+                username = "test-noti2",
+                password = "test-noti2",
+                providerType = ProviderType.LOCAL,
+            ),
+        )
+
+        // when
+        val response = request(
+            method = HttpMethod.POST,
+            url = "/api/admin/notifications",
+            body = NotificationBulkInsertDto(
+                content = listOf(
+                    NotificationRequestDto(
+                        content = "test",
+                        userId = user.id!!,
+                        link = "https://test.com",
+                    ),
+                    NotificationRequestDto(
+                        content = "test2",
+                        userId = user.id!!,
+                        link = "https://test.com",
+                    ),
+                ),
+            ),
+            isAdmin = true,
+        )
+
+        // then
+        response.andExpect(status().isOk)
+            .andExpect {
+                val notifications = findAll<Notification>(
+                    "SELECT n from Notification n WHERE n.user = :user",
+                    mapOf("user" to user),
+                )
+                notifications.size shouldBe 2
+            }
+    }
+}
