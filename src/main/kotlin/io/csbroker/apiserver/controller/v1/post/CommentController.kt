7efe9f22c98d3b0a281dd89f15a13d7c1e9ee052
kshired <shiroed1211@gmail.com>
2023-10-09 23:35:14 +0900

    fix : incorrect import

--- src/main/kotlin/io/csbroker/apiserver/controller/v1/post/CommentController.kt
+++ src/main/kotlin/io/csbroker/apiserver/controller/v1/post/CommentController.kt
@@ -3,8 +3,8 @@ package io.csbroker.apiserver.controller.v1.post
 import io.csbroker.apiserver.auth.LoginUser
 import io.csbroker.apiserver.controller.v1.post.request.CommentCreateRequestDto
 import io.csbroker.apiserver.dto.common.ApiResponse
+import io.csbroker.apiserver.model.User
 import io.csbroker.apiserver.service.post.CommentService
-import org.springframework.security.core.userdetails.User
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
