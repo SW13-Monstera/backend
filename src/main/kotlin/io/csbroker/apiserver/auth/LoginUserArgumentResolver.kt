bbd7dee0a74cbbb664304e455cbe44c12afb1e49
kshired <shiroed1211@gmail.com>
2023-11-26 11:41:11 +0900

    refactor : getEmailFromSecurityHolder function

--- src/main/kotlin/io/csbroker/apiserver/auth/LoginUserArgumentResolver.kt
+++ src/main/kotlin/io/csbroker/apiserver/auth/LoginUserArgumentResolver.kt
@@ -2,10 +2,10 @@ package io.csbroker.apiserver.auth
 
 import io.csbroker.apiserver.common.enums.ErrorCode
 import io.csbroker.apiserver.common.exception.UnAuthorizedException
+import io.csbroker.apiserver.common.util.getEmailFromSecurityContextHolder
 import io.csbroker.apiserver.model.User
 import io.csbroker.apiserver.repository.user.UserRepository
 import org.springframework.core.MethodParameter
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
@@ -27,14 +27,8 @@ class LoginUserArgumentResolver(
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?,
     ): Any {
-        val authentication = SecurityContextHolder.getContext().authentication
-
-        if (authentication == null || authentication.principal == "anonymousUser") {
-            throw UnAuthorizedException(ErrorCode.UNAUTHORIZED, "로그인이 필요합니다.")
-        }
-        val email = authentication.principal?.let {
-            it as? org.springframework.security.core.userdetails.User
-        }?.username ?: throw UnAuthorizedException(ErrorCode.UNAUTHORIZED, "로그인이 필요합니다.")
+        val email = getEmailFromSecurityContextHolder()
+            ?: throw UnAuthorizedException(ErrorCode.UNAUTHORIZED, "로그인이 필요합니다.")
 
         return userRepository.findByEmail(email)
             ?: throw UnAuthorizedException(ErrorCode.NOT_FOUND_ENTITY, "알 수 없는 유저의 요청입니다.")
