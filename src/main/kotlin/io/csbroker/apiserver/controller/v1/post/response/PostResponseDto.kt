57fa5bbd085534aee51027b5796389df3303b11d
SEONGILKIM <shiroed1211@naver.com>
2023-12-18 01:23:34 +0900

    feat : post와 comment 응답에 user id를 포함하도록 수정 (#167)
    * feat : post와 comment 응답에 user id를 포함하도록 수정
    
    * update restdocs

--- src/main/kotlin/io/csbroker/apiserver/controller/v1/post/response/PostResponseDto.kt
+++ src/main/kotlin/io/csbroker/apiserver/controller/v1/post/response/PostResponseDto.kt
@@ -1,11 +1,13 @@
 package io.csbroker.apiserver.controller.v1.post.response
 
 import io.csbroker.apiserver.model.Post
+import java.util.UUID
 
 data class PostResponseDto(
     val id: Long,
     val content: String,
     val username: String,
+    val userId: UUID,
     val likeCount: Long,
     val isLiked: Boolean,
     val comments: List<CommentResponseDto>,
@@ -13,9 +15,10 @@ data class PostResponseDto(
     constructor(post: Post, likeCount: Long, isLiked: Boolean, comments: List<CommentResponseDto>) : this(
         id = post.id,
         content = post.content,
+        userId = post.user.id!!,
         username = post.user.username,
         likeCount = likeCount,
         isLiked = isLiked,
-        comments = comments,
+        comments = comments.sortedBy { it.createdAt },
     )
 }
